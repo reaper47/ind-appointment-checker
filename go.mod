bcfd3e88d008830845878a6f0162ac4666de5fb4
reaper47 <macpoule@gmail.com>
2022-05-09 21:47:56 +0200

    removed unnecessary cobra

--- go.mod
+++ go.mod
@@ -5,5 +5,4 @@ go 1.16
 require (
 	github.com/go-co-op/gocron v1.13.0
 	github.com/joho/godotenv v1.4.0
-	github.com/spf13/cobra v1.4.0
 )
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/reaper47/ind-appointment-checker/cmd"
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/joho/godotenv"
+	"github.com/reaper47/ind-appointment-checker/internal/jobs"
+)
 
 func main() {
-	cmd.Execute()
+	exe, err := os.Executable()
+	if err != nil {
+		log.Fatal("could not get working dir:", err)
+	}
+
+	err = godotenv.Load(filepath.Dir(exe) + "/.env")
+	if err != nil {
+		log.Fatal("Error loading .env file:", err)
+	}
+
+	jobs.ScheduleCronJobs()
 }
